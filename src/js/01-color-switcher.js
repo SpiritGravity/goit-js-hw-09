0c1eb907f89497f983a800172fd5e0dd66c340c5
Anastasiia Tyshkevych <anastasia.tyshkevych@gmail.com>
2022-01-12 09:47:45 +0200

    add-first-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    bodyRef: document.querySelector('body'),
+};
 
+let timerId = null;
+const INTERVAL_DURATION = 1000;
+refs.startBtn.disabled = false;
+refs.stopBtn.disabled = true;
+
+function updateBodyBgColor (){
+    refs.bodyRef.style.backgroundColor =  getRandomHexColor();
+}  
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+    timerId = setInterval(updateBodyBgColor, INTERVAL_DURATION);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+
+    console.log("Change the background color");
+};
+
+function onStopBtnClick() {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    clearInterval(timerId);
+
+    console.log('Stop the timer');
+};
